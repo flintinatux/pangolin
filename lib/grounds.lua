027b5434c95b5dab70976754b9ac3f9ec4077e76
Scott McCormack <scott.mccormack@articulate.com>
2017-02-17 13:33:02 -0500

    Extract ground generation function

---
+++ lib/grounds.lua
@@ -0,0 +1,37 @@
+local config = require('lib.config')
+local Ground = require('entities.ground')
+
+local insert = table.insert
+local floor, pi, random, sin = math.floor, math.pi, math.random, math.sin
+
+local width, tile = config.map.width, config.tile
+
+local factor = function()
+  return random(-0.2, 0.2)
+end
+
+local genNoise = function()
+  local a, b, c, d, e = factor(), factor(), factor(), factor(), factor()
+  return function(x)
+    return a*sin(x) + b*sin(2*x) + c*sin(4*x) + d*sin(8*x) + e*sin(16*x)
+  end
+end
+
+return function()
+  local scale = 2 * pi / width
+  local noise = genNoise()
+  local grounds, ys = {}, {}
+
+  for x = 0, width - tile.w, tile.w do
+    insert(ys, floor(noise(scale * x) * 3) * tile.h)
+  end
+
+  for i = 1, #ys, 1 do
+    local l, r = ys[i], ys[i % #ys + 1]
+    local x = (i - 1) * tile.w
+    local y = min(l, r)
+    insert(grounds, Ground({ l = l, r = r, x = x, y = y }))
+  end
+
+  return grounds
+end
