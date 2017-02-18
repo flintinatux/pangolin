301b9b4d5a9ca7186e09b3165d1d8d04bef9ce57
Scott McCormack <scott.mccormack@articulate.com>
2017-02-17 21:49:10 -0500

    Extract ys

--- lib/grounds.lua
+++ lib/grounds.lua
@@ -2,29 +2,11 @@ local config = require('lib.config')
 local Ground = require('entities.ground')
 
 local insert = table.insert
-local ceil, pi, random, sin = math.ceil, math.pi, math.random, math.sin
 
-local width, tile = config.map.width, config.tile
+local tile = config.tile
 
-local factor = function()
-  return random(-0.2, 0.2)
-end
-
-local genNoise = function()
-  local a, b, c, d, e = factor(), factor(), factor(), factor(), factor()
-  return function(x)
-    return a*sin(x) + b*sin(2*x) + c*sin(4*x) + d*sin(8*x) + e*sin(16*x)
-  end
-end
-
-return function()
-  local scale = 2 * pi / width
-  local noise = genNoise()
-  local grounds, ys = {}, {}
-
-  for x = 0, width - tile.w, tile.w do
-    insert(ys, ceil(noise(scale * x) * 3) * tile.h)
-  end
+return function(ys)
+  local grounds = {}
 
   for i = 1, #ys, 1 do
     local l, r = ys[i], ys[i % #ys + 1]
--- states/forest.lua
+++ states/forest.lua
@@ -1,5 +1,6 @@
 local World   = require('lib.world')
 local config  = require('lib.config')
+local floor   = require('lib.floor')
 local grounds = require('lib.grounds')
 local Player  = require('entities.player')
 
@@ -9,8 +10,9 @@ local function Forest()
   local world = World()
 
   function world.entities()
+    local ys = floor()
     local player = Player({ x = -tile.w, y = -tile.h })
-    return concat(grounds(), { player })
+    return concat(grounds(ys), { player })
   end
 
   return world
